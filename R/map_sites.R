dfa61ecc2a135531251d322bd77e94d0ce202ca0
Dudley <rwdudley@usgs.gov>
2019-05-22 11:57:08 -0400

    Stuff we've pulled togther so far

---
+++ R/map_sites.R
@@ -0,0 +1,134 @@
+#' map_sites
+#'
+#' Returns map of data element mapped by sites that have lat-lons 
+#' Only works with one data element as currently written
+#' Assumes the s.wuds dataframe being used as input has already been subsetted 
+#' using filter functions (by site type, state, or county, years, etc.)
+#' that is, no subsetting is done inside this function
+#' Dataframe must not have more than one row per site ID
+#' Point sizes are scaled to value
+#' 
+#' @param s.wuds dataframe, the swuds water use data 
+#' @param data.element chr, data element to be plotted 
+#' @param year int, the year of interest to be mapped (defines historical basis for counties)
+#' @param state character name of state
+#' @param norm.element chr, element used to normalize data.element 
+#' @param unit.type chr, type of unit to be mapped; acceptable options are "county", "huc", or "aquifer"
+#' @param site.from.to chr, either 'from' or 'to' to designate the site lats-lons to use; the 'from' site or the 'to' site
+#'  
+#' @export
+#' 
+#' @import maps
+#' @import ggplot2
+#' @import maptools
+#' @import mapproj
+#' @import rgeos
+#' @import scales
+#' @importFrom ggthemes theme_map
+#' @importFrom dplyr left_join
+#' @importFrom scales pretty_breaks
+#' 
+#' @examples 
+#' s.wuds <- swudsSample #example data from OHIO
+#' s.wuds <- s.wuds[which(s.wuds$FROM_SITE_TP_CD == "AS" & s.wuds$YEAR == 2010 &s.wuds$Month_num == 8),]# subset for this example
+#' data.element <- "Volume_mgd"
+#' state <- "Ohio"
+#' year <- 2010
+#' unit.type <- "county"
+#' norm.element <- NA
+#' sites.map <- map_sites(s.wuds, data.element, norm.element)
+#' data.element <- "JUL_VAL"
+#' norm.element <- "ANNUAL_VAL"
+#' sites.map <- map_sites(s.wuds, data.element, norm.element)
+#' sites.map
+#' 
+map_sites <- function(s.wuds, data.element, year, state, norm.element=NA, unit.type="county", site.from.to = 'from'){
+  
+
+  if(unit.type=="county"){
+    # get county polygons
+    hc.sub <- subset_county_polygons(year, "STATE_TERR", state)
+    hc.subf<-fortify(hc.sub,region = "FIPS")
+    hc.sub@data$id<-hc.sub@data$FIPS
+
+  } else if (unit.type=="huc"){
+    #get huc polygons
+    state <- stateCd$STUSAB[which(stateCd$STATE_NAME == state)]
+    hc.sub <- subset_huc_polygons(year, "STATES", state)
+    hc.subf<-fortify(hc.sub,region = "HUC8")
+    hc.sub@data$id<-hc.sub@data$HUC8
+
+  } else if (unit.type=="aquifer"){
+    #get aquifer polygons
+  }
+  
+  hc.subf <- left_join(hc.subf,hc.sub@data, by="id")
+  
+  # coerce numeric class and normalize if a norm.element is specified
+  s.wuds[, data.element] <- as.numeric(s.wuds[, data.element])
+  if (!is.na(norm.element)){
+   # s.wuds[, norm.element] <- as.numeric(as.character(s.wuds[, norm.element]))
+    for (i in data.element){
+      s.wuds[,paste0(i,"_norm")] <- s.wuds[,i] / s.wuds[,norm.element]
+    }
+  }
+  
+  # element to be plotted
+  ifelse(is.na(norm.element), p.elem <- data.element, p.elem <- paste0(data.element,"_norm"))
+  
+  if(all(is.na(s.wuds[, p.elem]))) stop('No data available.')
+  
+  # check for multiple values per station
+  
+  
+
+  # set common key name "id" to merge water use data with polygon data
+  if (unit.type=="county") {names(s.wuds)[names(s.wuds)=="STATECOUNTYCODE"] <- "id"}
+  if (unit.type=="huc") {names(s.wuds)[names(s.wuds)=="HUCCODE"] <- "id"}
+  
+  # merge polygons and water use data 
+  hc.subf <- left_join(hc.subf,s.wuds, by="id")
+  
+  # plot element
+  p.elem <- data.element
+  
+  if(all(is.na(s.wuds[p.elem]))) stop('No data available.')
+  
+  if (!is.na(norm.element)){p.elem <- paste0(data.elements,"_norm")}
+  
+  if("COUNTYNAME" %in% names(s.wuds) & !("COUNTYNAME" %in% names(hc.subf))){
+    hc.subf <- left_join(hc.subf, s.wuds[,c("STATECOUNTYCODE", "COUNTYNAME")], by=c("id"="STATECOUNTYCODE"))
+    hc.subf$labels <- paste("Area:",hc.subf$COUNTYNAME, "\n",p.elem,":",hc.subf[[p.elem]])
+  } else if(unit.type=="county" & "Area.Name" %in% names(s.wuds) & !("Area.Name" %in% names(hc.subf))) {
+    hc.subf <- left_join(hc.subf, s.wuds[,c("STATECOUNTYCODE", "Area.Name")], by=c("id"="STATECOUNTYCODE"))
+    hc.subf$labels <- paste("Area:",hc.subf$Area.Name, "\n",p.elem,":",hc.subf[[p.elem]])
+  } else if(unit.type=="huc" & "Area.Name" %in% names(s.wuds) & !("Area.Name" %in% names(hc.subf))) {
+    hc.subf <- left_join(hc.subf, s.wuds[,c("HUCCODE", "Area.Name")], by=c("id"="HUCCODE"))
+    hc.subf$labels <- paste("Area:",hc.subf$Area.Name, "\n",p.elem,":",hc.subf[[p.elem]])
+  } else {
+    hc.subf$labels <- paste("Area:",hc.subf$group, "\n",p.elem,":",hc.subf[[p.elem]])    
+  }
+  
+  hc.subf <- hc.subf[order(hc.subf$order), ]
+  hc.subf <- unique(hc.subf[,c("long","lat","group","labels",p.elem)])
+  hc.subf <- hc.subf[!is.na(hc.subf[,p.elem]),]
+  
+  
+  
+  
+  ch.plot <- ggplot() + geom_polygon(data = hc.subf, 
+                                     aes_string(x = "long", y = "lat", 
+                                                group="group", fill= p.elem, label = "labels"),#hc.subf[,p.elem]), 
+                                     color="black", size=0.25) + 
+    coord_quickmap() + 
+    theme_map() +
+    theme(legend.position=c(.8, .2)) +
+    scale_fill_distiller(name=p.elem, palette = "YlGn", 
+                         breaks = pretty_breaks(n = 5), trans = "reverse")
+  
+  print(ch.plot)
+  
+  
+  return(sites.map)
+  
+}
---
+++ R/swuds_parse_functions.R
@@ -0,0 +1,121 @@
+library(readxl)
+
+# Function to read in SWUDS Water Quantity Excel file into a dataframe
+# and converts column names to NWIS codes using a lookup table nwis_lookup.xlsx
+#-------------------------
+readWaterQuantityXL <- function(file_path){
+  
+  dq <- readxl::read_xlsx(path = file_path)
+  
+  # convert column names to NWIS names 
+  nwisLU <- readxl::read_xlsx("./inst/extdata/nwis_lookup.xlsx")
+  
+  swudcols <- nwisLU$swuds
+  NWIScols <- nwisLU$nwis
+  
+  for (sc in swudcols){
+    colnames(dq)[colnames(dq) == sc] <- NWIScols[which(swudcols == sc)]
+  }
+  
+  
+  return(dq)
+}
+#--------------------------
+
+# Function to read in SWUDS Population Served Excel file into a dataframe
+# and converts column names to NWIS codes using a lookup table nwis_lookup.xlsx
+#--------------------------
+readPopServedXL <- function(file_path){
+  
+  dp <- readxl::read_xlsx(path = file_path)
+  
+  # convert column names to NWIS names 
+  nwisLU <- readxl::read_xlsx("./inst/extdata/nwis_lookup.xlsx")
+  
+  swudcols <- nwisLU$swuds
+  NWIScols <- nwisLU$nwis
+  
+  for (sc in swudcols){
+    colnames(dp)[colnames(dp) == sc] <- NWIScols[which(swudcols == sc)]
+  }
+  
+  return(dp)
+  
+  
+}
+#---------------------------
+
+# Function to merge Water Quantity and Population Served data frames into one
+# by "to_agency code", "to_site_no", "Year"
+#---------------------------
+mergeWaterQuantPopServ <- function(waterQuantDF, popServDF){
+  
+  df <- dplyr::left_join(waterQuantDF, popServDF, by = c("TO_AGENCY_CD", "TO_SITE_NO", "YEAR"))
+  
+  return(df)
+  
+}
+#---------------------------
+
+# Function to create month, year, month#, day, date, decimal date, and water
+# year to the mergeWaterQuantPopServe data frame
+#---------------------------
+meltWaterQuantPopServ <- function(mergeWaterQuantPopServ){
+  
+  df_melt<- plyr::rename(df, c("JAN_VAL" = "Jan", "FEB_VAL" = "Feb", "MAR_VAL" = "Mar", "APR_VAL" = "Apr", "MAY_VAL" = "May",
+                                 "JUN_VAL" = "Jun", "JUL_VAL" = "Jul", "AUG_VAL" = "Aug", "SEP_VAL" = "Sep", "OCT_VAL" = "Oct", "NOV_VAL" = "Nov", 
+                                 "DEC_VAL" = "Dec"))
+  
+  df_melt <- df_melt %>%
+    gather(Month, Volume_mgd, Jan:Dec)
+  
+  df_melt$Month_num <- match(df_melt$Month, month.abb)
+  df_melt$Month_num <- str_pad(df_melt$Month_num, width = 2, side = "left", pad = "0")
+  
+  df_melt$date <- paste(df_melt$YEAR, df_melt$Month_num, "01", sep = "-")
+  df_melt$Day <- days_in_month(as.Date(df_melt$date))
+  df_melt$date <- NULL
+  df_melt$Date <- paste(df_melt$YEAR, df_melt$Month_num, df_melt$Day, sep = "-")
+  df_melt$dec_date <- decimal_date(as.Date(df_melt$Date))
+  
+  wtr_yr <- function(dates, start_month=9) {
+    # Convert dates into POSIXlt
+    dates.posix = as.POSIXlt(dates)
+    # Year offset
+    offset = ifelse(dates.posix$mon >= start_month - 1, 1, 0)
+    # Water year
+    adj.year = dates.posix$year + 1900 + offset
+    # Return the water year
+    adj.year
+  }
+  
+  df_melt$water_year <- wtr_yr(df_melt$Date)
+  
+  # repair column names 
+  colnames(df_melt) <- gsub(pattern = '+', replacement = '_', x = colnames(df_melt), fixed = T)
+  colnames(df_melt) <- gsub(pattern = ' ', replacement = '_', x = colnames(df_melt), fixed = T)
+  
+  # change character "NA" to real NA values
+  df_melt$Volume_mgd[df_melt$Volume_mgd=="NA"] <- NA
+  #df_melt[df_melt=="NA"] <- NA
+  
+  # enforce numeric
+  df_melt$ANNUAL_VAL <- as.numeric(df_melt$ANNUAL_VAL)
+  df_melt$Month_num <- as.numeric(df_melt$Month_num)
+  df_melt$Volume_mgd <- as.numeric(df_melt$Volume_mgd)
+  df_melt$Day <- as.numeric(df_melt$Day)
+  df_melt$dec_date <- as.numeric(df_melt$dec_date)
+  df_melt$water_year <- as.numeric(df_melt$water_year)
+  df_melt$YEAR <- as.numeric(df_melt$YEAR)
+  df_melt$TO_DEC_LAT_VA <- as.numeric(df_melt$TO_DEC_LAT_VA)
+  df_melt$FROM_DEC_LAT_VA <- as.numeric(df_melt$FROM_DEC_LAT_VA)
+  df_melt$TO_DEC_LONG_VA <- as.numeric(df_melt$TO_DEC_LONG_VA)
+  df_melt$FROM_DEC_LONG_VA <- as.numeric(df_melt$FROM_DEC_LONG_VA)
+  df_melt$Annual_Population <- as.numeric(df_melt$Annual_Population)
+  
+  
+
+  return(df_melt)
+}
+
+
