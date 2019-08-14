421945b8ad348df8ade59d69ebc5aef220bac172
Laura <ldecicco@usgs.gov>
2019-08-14 14:57:39 -0500

    Skip the map for now...adding a shiny app

---
+++ inst/shiny/server.R
@@ -0,0 +1,20 @@
+options(shiny.maxRequestSize=10*1024^2)
+
+
+shiny::shinyServer(function(input, output, session) {
+  
+  observe({
+    if (input$close > 0) shiny::stopApp()    
+  })
+  
+  output$mymap <- leaflet::renderLeaflet({
+    
+    isolate({
+      map <- leaflet::leaflet() %>%
+        leaflet::addProviderTiles("CartoDB.Positron") %>%
+        leaflet::setView(lng = -83.5, lat = 44.5, zoom=6)    
+    })
+    
+  })
+  
+})
